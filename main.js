385062d40285d10a46bda89be0e6738ae7a3b17d
AllanLimitless <allanlimitless55@gmail.com>
2020-04-14 07:47:22 -0400

    main.js Update
    Setting up an event listener for the button

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 $(document).ready(function () {
 
   //********** Quotes Section  **********//
+ 
+  displayQuoteFromArray();
+
+  // When #nextQuote button is clicked, generate new quote   
+  $("#nextQuote").click(function () {
+    // fadeOut() method gradually changes the opacity, from visible to hidden (fading effect)
+    $('#main').fadeOut();
+    getRandomQuote();
+  });
 
 
 }); // End document ready
