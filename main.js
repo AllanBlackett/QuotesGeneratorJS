5e7fd677a6cba8f9589366c0b1412622ec19d089
AllanLimitless <allanlimitless55@gmail.com>
2020-04-15 08:17:02 -0400

    Update
    Added function for grabbing the quotes using Ajax call

--- main.js
+++ main.js
@@ -12,5 +12,35 @@ $(document).ready(function () {
     getRandomQuote();
   });
 
+  
+  // Grabbing the quotes using Ajax call
+  function getRandomQuote() {
+    $.ajax({
+      type: "POST",
+      url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+      dataType: "json",
+      success: displayAPIQuote,
+      // display quotes from an displayQuoteFromArray array if AJAX call fails
+      error: displayQuoteFromArray,
+      beforeSend: setHeader,
+    });
+
+    // setting what kind of response it will return
+    function setHeader(xhr) {
+      xhr.setRequestHeader("X-Mashape-Key", "XEmG41b3OumshAGz4MeTGVfhZNmdp1Cty6ajsnAKusfLZ0iZKC");
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.setRequestHeader("Accept", "application/json");
+    }
+  }
+
+  // display quotes from API call
+  function displayAPIQuote(response) {
+    console.log(response[0].quote);
+    // Displaying results
+    $('#quote').text(response[0].quote);
+    $('#author').text(response[0].author);
+    $('#main').fadeIn(1200);
+  }
+
 
 }); // End document ready
